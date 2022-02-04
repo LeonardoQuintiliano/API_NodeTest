86580f49917e0d23762d5eb3bf29d6bef6abdf4b
LeonardoQuintiliano <leoqds18@gmail.com>
2022-02-04 00:28:17 -0300

    feature - criando rota de autenticação

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require('bcryptjs');
 
 const User = require("../models/user");
 
@@ -17,5 +18,23 @@ router.post('/register', async (req, res) => {
         return res.status(400).send({ error: 'Registration failed' });
     }
 });
+
+router.post('/autenticate', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email }).select('+password');
+
+        if (!user)
+            return res.status(400).send({ error: 'User not found' });
+
+        if (!await bcrypt.compare(password, user.password))
+            return res.status(400).send({ error: 'Invalid password' });
+
+        res.send({ user });
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 module.exports = app => app.use("/auth", router);
